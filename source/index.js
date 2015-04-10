530449c7dca97da494b8183dceb28439b356492a
Kyle Mitchell <kyle@kemitchell.com>
2015-04-10 08:54:28 +0000

    refactor

--- source/index.js
+++ source/index.js
@@ -1,32 +1,22 @@
-var version = require('commonform-version');
-var validate = require('commonform-validate');
 var Immutable = require('immutable');
 var isMap = Immutable.Map.isMap.bind(Immutable.Map);
+var validate = require('commonform-validate');
+var version = require('commonform-version');
 
-var digest = validate.digest.bind(validate);
 var term = validate.term.bind(validate);
 
-var bookmarkName = exports.bookmarkName = function(argument) {
-  return (
-    term(argument) &&
-    argument.indexOf('@') < 0
-  );
-};
-
-var propertyIs = function(argument, key, predicate) {
-  return (
-    argument.has(key) &&
-    predicate(argument.get(key))
-  );
+var props = {
+  form: validate.digest.bind(validate),
+  name: function(argument) {
+    return term(argument) && argument.indexOf('@') < 0;
+  },
+  version: version
 };
 
 module.exports = function(argument) {
-  return (
-    isMap(argument) &&
-    propertyIs(argument, 'version', version) &&
-    propertyIs(argument, 'name', bookmarkName) &&
-    propertyIs(argument, 'form', digest)
-  );
+  return isMap(argument) && Object.keys(props).every(function(key) {
+    return argument.has(key) && props[key](argument.get(key));
+  });
 };
 
 module.exports.version = '1.0.0';
